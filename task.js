22ff5e6535b88e2947797594feffd4847a0d2abe
AleksandrEpishkin <aepishkin@yandex.ru>
2022-09-11 19:45:01 +0300

    task2 & optimized task1

--- task.js
+++ task.js
@@ -1,30 +1,35 @@
 "use strict";
 function solveEquation(a, b, c) {
-  let arr
+  let arr = [];
   let D = Math.pow(b, 2) - 4 * a * c;
-  if (D < 0)
-    return [];
   if (D == 0)
-    arr = [(-b + Math.sqrt(D)) / (2 * a)];
+    arr.push((-b + Math.sqrt(D)) / (2 * a));
   else if (D > 0) {
-    let tmp = [];
-    tmp.push((-b + Math.sqrt(D)) / (2 * a));
-    tmp.push((-b - Math.sqrt(D)) / (2 * a));
-    arr = tmp;
-  }
-  return arr; // array
+    arr.push((-b + Math.sqrt(D)) / (2 * a));
+    arr.push((-b - Math.sqrt(D)) / (2 * a));
+  };
+  return arr;
 }
 
 
 "use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-//   let totalAmount;
-//   let mounts = date / (new Date().setFullYear(new Date().getFullYear()));
-//   let amountCredit = amount - contribution;
-//   if (amountCredit <= 0)
-//     return 0;
-//   let monthlyRate = percent / (100 * 12);
-//   let monthlyPayment = amountCredit * (monthlyRate + (monthlyRate / (Math.pow((1 + monthlyRate), mounts) - 1)));
-//   totalAmount = monthlyPayment * mounts;
-//   return totalAmount.toFixed(2);
-// }
+  if (isNaN(percent)) {
+    return (`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`)
+  };
+  if (isNaN(contribution)) {
+    return (`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`)
+  };
+  if (isNaN(amount)) {
+    return (`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`)
+  };
+  let totalAmount;
+  let mounts = ((date - (new Date(new Date().setFullYear(new Date().getFullYear())))) / 2629800000).toFixed();
+  let amountCredit = amount - contribution;
+  if (amountCredit <= 0)
+    return 0;
+  let monthlyRate = percent / (100 * 12);
+  let monthlyPayment = amountCredit * (monthlyRate + (monthlyRate / (Math.pow((1 + monthlyRate), mounts) - 1)));
+  totalAmount = +(monthlyPayment * mounts);
+  return +totalAmount.toFixed(2);
+}
